c98ca97897febf9709726b8ace716fdf7a89681a
yudevdev <yuyadev555@gmail.com>
2025-03-17 15:30:17 +0900

    [update] StrictModeの指定方法を変更

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import {createRoot} from 'react-dom/client';
 import App from "./App";
-import * as React from 'react';
+import { StrictMode } from 'react';
 
 const root = createRoot(document.getElementById("root")!)
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
